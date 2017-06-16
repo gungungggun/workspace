e54816b0ba46d281a51a445ce0ec1f6e992a7266
gun <gungungggun@gmail.com>
2017-06-16 12:00:31 +0900

    いろいろ

--- vue-practice/src/components/Result1.vue
+++ vue-practice/src/components/Result1.vue
@@ -1,28 +1,23 @@
 <template lang="pug">
-  div#result
-    div.simple
-      div.place
-        span 甲子園球場
-      div.middle
-        div.team
-          div.left(v-bind:class="{win: result.teamA > result.teamB && score, lose: result.teamA < result.teamB && score}")
-            span 報徳学園
-          div.right(v-bind:class="{win: result.teamA < result.teamB && score, lose: result.teamA > result.teamB && score}")
-            span 早稲田実業
-        div.see(v-on:click="scoreCumpute()")
-          span.view(v-bind:class="{result: score}") 試合結果を見る
-        transition(name="result")
-          div.score(v-if="score")
-            span.s-left(v-bind:class="{win: result.teamA > result.teamB && score, lose: result.teamA < result.teamB && score}") {{ result.teamA }}
-            span.s-right(v-bind:class="{win: result.teamA < result.teamB && score, lose: result.teamA > result.teamB && score}") {{ result.teamB }}
-
-      div.wrapHero(v-bind:class="{show: hero}")
-        transition(name="slide-in")
-          result1
+      div.hero(v-if="score")
+        input#hero(type="checkbox" v-model="hero")
+        label(for="hero")
+          div.arw
+            i.fa-arw.arw1
+            i.fa-arw.arw2
+            i.fa-arw.arw3
+          span Today's hero
+        transition(name="bounce")
+          div.box(v-if="hero" v-bind:class="{boxSelect: boxSelect}")
+            div.elm(v-on:click="heroCompute(0)" v-bind:class="{select: select == 0}")
+              img(src="../assets/img_man1.jpg")
+            div.elm(v-on:click="heroCompute(1)" v-bind:class="{select: select == 1}")
+              img(src="../assets/img_man2.jpg")
+            div.elm(v-on:click="heroCompute(2)" v-bind:class="{select: select == 2}")
+              img(src="../assets/img_man3.jpg")
 </template>
 
 <script>
-import Result1 from '@/components/Result1'
 export default {
   name: 'result',
   data () {
@@ -38,9 +33,6 @@ export default {
         teamB: 1
       }
     }
-  },
-  components: {
-    'result1': Result1
   },
   methods: {
     scoreCumpute: function () {
@@ -107,88 +99,6 @@ blinking(count=infinite)
   src url('../assets/webfonts/fontawesome-webfont.ttf')
 
 #result
-  font-family 'nico'
-  padding 2%
-.simple
-  background #fff
-  border 3px solid #777
-  position relative
-  .middle
-    position relative
-    .team
-      clearfix()
-      .left
-        float left
-        background #00A3D8
-        color #fff
-        width:50%
-        padding 5% 0
-        &.lose
-          // background #167796
-        span
-          font-size 4vw
-          padding-right 30%
-      .right
-        float right
-        background #D7183E
-        color #fff
-        width:50%
-        padding 5% 0
-        transition 1s
-        &.lose
-          // background #881028
-        span
-          font-size 4vw
-          padding-left 30%
-    .see
-      position absolute
-      top 0
-      width 100%
-      padding 4% 0
-      cursor pointer
-      span.view
-        overflow hidden
-        display block
-        border 1px solid #fff
-        font-size 4vw
-        margin 0 auto
-        width 38%
-        height 7vw
-        line-height 7.5vw
-        color #fff
-        background #18a249
-        transition 0.5s
-        position relative
-        &.result
-          top: 3.5vw
-          width 2vw
-          height 0
-          transform rotateZ(720deg);
-    .score
-      position absolute
-      top 0
-      width 100%
-      .s-left
-        display inline-block
-        font-size 11vw
-        color #fff
-        padding-right 8%
-        &.win
-          color #F7CF58
-      .s-right
-        display inline-block
-        font-size 11vw
-        color #fff
-        padding-left 8%
-        padding-top 1%
-        &.win
-          color #F7CF58
-  .place
-    background #F7CF58
-    padding 2%
-    span
-      font-size 4vw
-      color #000
   .wrapHero
     overflow hidden
     max-height 8vw
--- vue-practice/src/components/Scene1.vue
+++ vue-practice/src/components/Scene1.vue
@@ -2,18 +2,60 @@
   div#scene1
     h1 Scene1
     router-link(to="/scene2") Scene2にいくよ！
+
+    draggable.drag(w-model="arr1" :options="{group: 'test'}" @change="log")
+      div(v-for="element in arr1") {{element.name}}
+
+    draggable.drag(v-model="arr2" :options="{group: 'test'}")
+      div(v-for="element in arr2") {{element.name}}
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 export default {
   name: 'scene1',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      arr1: [
+        {
+          name: 'test1'
+        },
+        {
+          name: 'test2'
+        },
+        {
+          name: 'test3'
+        },
+        {
+          name: 'test4'
+        }
+      ],
+      arr2: [
+        {
+          name: 'test5'
+        },
+        {
+          name: 'test6'
+        }
+      ]
+    }
+  },
+  components: {
+    draggable
+  },
+  methods: {
+    log: function (evt) {
+      console.log(evt)
     }
   }
 }
 </script>
 
 <style lang="stylus">
+.drag
+  padding 10px
+  div
+    padding 10px
+    border 1px solid #555
 </style>
