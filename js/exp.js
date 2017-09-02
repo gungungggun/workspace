ad87347f474555fa9302244bb38a8fa9e1c28f16
gun <gungungggun@gmail.com>
2017-09-02 09:51:00 +0900

    試し

--- js/exp.js
+++ js/exp.js
@@ -1,8 +1,11 @@
 var exp = '<div><a href="test"></a></div><div><a href="https://abc.com/test"></a></div>'
+exp = '<div></div>'
 
 m = exp.match(/<a(.|\s)*?>/gi)
 
-m.forEach(v => {
-  a = v.match(/href="([^\"]+)"/)
-  console.log(a[1])
-})
+if (m !== null) {
+  m.forEach(v => {
+    a = v.match(/href="([^\"]+)"/)
+    console.log(a[1])
+  })
+}
