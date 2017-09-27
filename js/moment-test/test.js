e1da41fb37ec2784f724bbedf112af6cd1236b0b
gun <gungungggun@gmail.com>
2017-09-27 19:42:19 +0900

    momentの試し

---
+++ js/moment-test/test.js
@@ -0,0 +1,9 @@
+let moment = require('moment')
+let d = moment(1506490411933)
+console.log(d)
+
+const now = new Date().getTime()
+console.log(now)
+
+const dd = new Date('2016-10-22').getTime()
+console.log(dd)
