4616e277ff4ec80843bf30fb7ebe601456a71a4c
gun <gungungggun@gmail.com>
2017-10-03 23:35:29 +0900

    includesなどのためし

---
+++ js/array/includes.js
@@ -0,0 +1,17 @@
+let arr = [1, 2, 3, 4, 6, 9, 5]
+
+console.log(arr.includes(1))
+console.log(arr.includes(2))
+console.log(arr.includes(3))
+console.log(arr.includes(4))
+console.log(arr.includes(5))
+console.log(arr.includes(6))
+console.log(arr.includes(7))
+
+console.log(arr.indexOf(1))
+console.log(arr.indexOf(2))
+console.log(arr.indexOf(3))
+console.log(arr.indexOf(4))
+console.log(arr.indexOf(5))
+console.log(arr.indexOf(6))
+console.log(arr.indexOf(7))
