0f85d2e72c6ecf190c0b81b5bf6f175bf8f6ed12
gun <gungungggun@gmail.com>
2017-11-03 14:55:46 +0900

    修正

--- js/asyncawait/loop.js
+++ js/asyncawait/loop.js
@@ -1,10 +1,10 @@
 function loop(i) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
       console.log(i)
       resolve(i+1)
     }, Math.floor( Math.random() * 50 ) + 50)
-  }).then(function(count) {
+  }).then((count) => {
     if (count > 10) {
     } else {
       loop(count)
