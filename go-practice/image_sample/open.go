2025024d6eb86bf8a269924719dc935f4f50b9f4
gun <gungungggun@gmail.com>
2017-11-05 23:14:46 +0900

    goで画像を扱う

---
+++ go-practice/image_sample/open.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	_ "image/png"
+	"os"
+)
+
+func main() {
+	file, _ := os.Open("./sample.png")
+	defer file.Close()
+
+	config, formatName, err := image.DecodeConfig(file)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(formatName)
+	fmt.Println(config.Width)
+	fmt.Println(config.Height)
+}
