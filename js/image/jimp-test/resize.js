e8b8dd4bfc0e92d00352de2033ba515e0c3552f1
gun <gungungggun@gmail.com>
2017-11-08 17:32:11 +0900

    resizeに大きさのifいれたバージョン

--- js/image/jimp-test/resize.js
+++ js/image/jimp-test/resize.js
@@ -14,3 +14,15 @@ Jimp.read('../samples/sample-tate.png', (err, image) => {
   image.resize(Jimp.AUTO, 48)
     .write('sample-tate-resize.jpg')
 })
+
+Jimp.read('../samples/sample-yoko.png', (err, image) => {
+  let width = 48
+  let height = 48
+  if (image.bitmap.width > image.bitmap.height) {
+    height = Jimp.AUTO
+  } else {
+    width = Jimp.AUTO
+  }
+  image.resize(width, height)
+    .write('sample-yoko-auto-resize.jpg')
+})
