05e25201db646e75a31ee209072304d4dc0bd3c5
gun <gungungggun@gmail.com>
2017-12-07 15:31:12 +0900

    jsで年月日

--- js/timestring.js
+++ js/timestring.js
@@ -2,3 +2,11 @@ let time = new Date().getTime().toString(16)
 console.log(time)
 
 console.log(Date.now().toString(16))
+
+let d = new Date()
+let year = d.getFullYear()
+let month = ('0' + (d.getMonth() + 1)).slice(-2)
+let day = ('0' + d.getDate()).slice(-2)
+
+let date = '' + year + month + day
+console.log(date)
