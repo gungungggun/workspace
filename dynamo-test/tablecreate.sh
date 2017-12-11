1f9b701c6cfa1bd162bb474c4bbee4592f3a1a3d
gun <gungungggun@gmail.com>
2017-12-11 12:01:36 +0900

    dynamo試し

--- dynamo-test/tablecreate.sh
+++ dynamo-test/tablecreate.sh
@@ -1,4 +1,9 @@
-v1="file://"$(cd $(dirname $0) && pwd)"/table.json"
-
+v1="file://"$(cd $(dirname $0) && pwd)"/table1.json"
 aws dynamodb create-table \
---cli-input-json $v1
+--cli-input-json $v1 \
+--endpoint-url http://localhost:8000
+
+v2="file://"$(cd $(dirname $0) && pwd)"/table2.json"
+aws dynamodb create-table \
+--cli-input-json $v2 \
+--endpoint-url http://localhost:8000
