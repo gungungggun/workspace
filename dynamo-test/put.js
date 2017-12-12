fcf6c9195dbb3e7056cf06a8b64a0bb4e288c7ba
gun <gungungggun@gmail.com>
2017-12-12 17:06:09 +0900

    dynamo put

--- dynamo-test/put.js
+++ dynamo-test/put.js
@@ -1,17 +1,48 @@
-let AWS = require('aws-sdk')
-let documentClient = new AWS.DynamoDB.DocumentClient()
+const AWS = require('aws-sdk')
+const dc = new AWS.DynamoDB.DocumentClient({
+  region: 'ap-northeast-1',
+  endpoint: new AWS.Endpoint('http://localhost:8000')
+})
 
-exports.handler = (event, context, callback) => {
-  let put = {
-    "TableName": "mock",
-    "Item": {
-      "id": "6",
-      "date": "00l0edo0ewo",
-      "age": 30,
-      "bool": false
-    }
-  }
-  documentClient.put(put, (err, data) => {
-    console.log(err)
-  })
+let param = {
+  TableName: 'mock1',
+  Item: {
+    id: '0005',
+    name: 'Goro',
+    age: 15
+  },
+  ReturnValues: 'ALL_NEW'
 }
+
+dc.put(param, (err, data) => {
+  console.log(data)
+  console.log(err)
+})
+
+/*
+const dc = new AWS.DynamoDB({
+  region: 'ap-northeast-1',
+  endpoint: new AWS.Endpoint('http://localhost:8000')
+})
+
+let param = {
+  TableName: 'mock1',
+  Item: {
+    id: {
+      S: '0005'
+    },
+    name: {
+      S: 'GOOO'
+    },
+    age: {
+      N: '10'
+    }
+  },
+  ReturnValues: 'ALL_NEW'
+}
+
+dc.putItem(param, (err, data) => {
+  console.log(data)
+  console.log(err)
+})
+*/
