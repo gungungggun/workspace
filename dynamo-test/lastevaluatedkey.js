6e3a8e00c967ef469bb024f48ce438afd0045d03
gun <gungungggun@gmail.com>
2018-01-28 16:54:08 +0900

    LastEvaluatedKey試し

---
+++ dynamo-test/lastevaluatedkey.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk')
+const dc = new AWS.DynamoDB.DocumentClient({
+  region: 'ap-northeast-1',
+  endpoint: new AWS.Endpoint('http://localhost:8000')
+})
+
+let param = {
+  TableName: 'mock1',
+  ExclusiveStartKey: {
+    id: '2'
+  },
+  Limit: 10
+}
+
+dc.scan(param, (err, data) => {
+  console.log(data)
+  console.log(err)
+})
