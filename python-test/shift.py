e4ad9b5ebff95dd8d1d418144f9c1aaee29e5ebc
gun <gungungggun@gmail.com>
2018-01-31 14:55:23 +0900

    pythonシフト演算

---
+++ python-test/shift.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+print('1 << 1')
+print(1 << 1)
+print(bin(1 << 1))
+print('\n')
+
+print('1 << 2')
+print(1 << 2)
+print(bin(1 << 2))
+print('\n')
+
+print('1 << 3')
+print(1 << 3)
+print(bin(1 << 3))
+print('\n')
+
+print('--------')
+
+print('1 << 1')
+print(1 << 1)
+print(bin(1 << 1))
+print('\n')
+
+print('2 << 1')
+print(2 << 1)
+print(bin(2 << 1))
+print('\n')
+
+print('3 << 1')
+print(3 << 1)
+print(bin(3 << 1))
+print('\n')
