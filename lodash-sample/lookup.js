839b4091e777103d5683b9517d7e444636c351f0
gun <gungungggun@gmail.com>
2018-03-07 11:42:04 +0900

    lodash

--- lodash-sample/lookup.js
+++ lodash-sample/lookup.js
@@ -8,5 +8,10 @@ let test = {
    }
 }
 
+if (test && test.aaa && test.aaa.ddd) {
+  console.log(test.aaa.bbb.ddd)
+}
+
 console.log(_.get(test, 'aaa.bbb.ccc'))
 console.log(_.get(test, 'aaa.bbb.ddd'))
+console.log(_.get(test, 'aaa.ddd.eee'))
