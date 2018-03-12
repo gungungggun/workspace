742d9af47d47181de84aeea265f03382e86cbb4c
gun <gungungggun@gmail.com>
2018-03-12 19:43:21 +0900

    faker試し

--- php-test/faker/src/MyFaker.php
+++ php-test/faker/src/MyFaker.php
@@ -4,3 +4,10 @@ include('../vendor/autoload.php');
 
 $faker = Faker\Factory::create();
 echo $faker->name;
+echo PHP_EOL;
+echo $faker->address;
+echo PHP_EOL;
+echo $faker->text;
+echo PHP_EOL;
+echo $faker->email;
+echo PHP_EOL;
