adc4e95698fb19e29727d6f743b58c78a3ebd827
gun <gungungggun@gmail.com>
2018-06-13 18:53:47 +0900

    git diffの試し2.11からデフォルトになったcompactionHeuristic

--- git-test/foo.rb
+++ git-test/foo.rb
@@ -1,5 +1,9 @@
 def finalize(values)
     values.each do |v|
+        v.prepare
+    end
+
+    values.each do |v|
         v.finalize
     end
 end
--- git-test/js.js
+++ git-test/js.js
@@ -1,5 +1,8 @@
 var a = 1
 
 if (a == 1) {
+  console.log(0)
+}
+if (a == 1) {
   console.log(1)
 }
