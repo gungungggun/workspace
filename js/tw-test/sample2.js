1ac04e3d5fa0099a4c8eee694f4734876667926c
gun <gungungggun@gmail.com>
2018-07-13 12:10:52 +0900

    Twitter API 1件取得

---
+++ js/tw-test/sample2.js
@@ -0,0 +1,17 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+
+const id = ''
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/show/' + id, params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
