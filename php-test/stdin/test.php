79e0f0322a6b17632867908c555acee194336fc0
gun <gungungggun@gmail.com>
2018-11-22 14:41:46 +0900

    phpで標準入力

---
+++ php-test/stdin/test.php
@@ -0,0 +1,12 @@
+<?php
+$stdins = array();
+while(true)
+{
+    $stdin = trim(fgets(STDIN));
+    if ($stdin === '')
+    {
+        var_dump($stdins);
+        return;
+    }
+    $stdins[] = $stdin;
+}
