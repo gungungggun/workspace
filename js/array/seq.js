6b2da0d0cb20dd22b82c131250ced3b23dafca06
gun <gungungggun@gmail.com>
2019-03-26 10:11:13 +0900

    連続

--- js/array/seq.js
+++ js/array/seq.js
@@ -1,5 +1,8 @@
-const a = [...Array(10).keys()].map(i => ++i)
+const a = [...Array(10).keys()]
 console.log(a)
 
-const b = [...Array(10).keys()].map(i => i * 0.5)
+const b = [...Array(10).keys()].map(i => ++i)
 console.log(b)
+
+const c = [...Array(10).keys()].map(i => i * 0.5)
+console.log(c)
